35801ccf34a05e16ae0eedd26b170013f795f09e
busrakarausta <bus_ra_893@hotmail.com>
2019-04-28 18:04:33 +0300

    Login and Register pages are implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Router, Route } from "react-router";
+import { createBrowserHistory } from "history";
+import "./App.css";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="jumbotron">
+      <div className="container">
+        <div className="col-sm-8 col-sm-offset-2">
+          <Router history={createBrowserHistory()}>
+            <div>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </div>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
